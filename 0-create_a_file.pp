ebb8a2dcb824deda87ebd6b7a61c4acf93becfeb
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-04-03 23:49:48 +0100

    Delete 0-create_a_file.pp

--- 0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# manifest to create file in tmp directory
-file { '/tmp/schoo':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'ww-data',
-  content => 'I love Puppet',
-}
