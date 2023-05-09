0e312d069be5fae413be017fb32c7b25ca3ffba0
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-05-09 21:37:49 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+@author: Bolakale Aduloju
 """
 
 from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
+    """ Queries to Reddit API """
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {'User-agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
