d13ef047c86f027fcdb9837b68124e79497eb24b
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-05-09 21:52:00 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
+
 """
-A function that queries the Reddit API and prints 
-the titles of the first 10 hot posts listed for a given subreddit.
-@authour: Bolakale Aduloju
+prints the titles of the first 10 hot posts listed for a given subreddit
 """
 
 from requests import get
 
 
 def top_ten(subreddit):
-  """
-  Queries to Reddit API
-  """
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
-    user_agent = {
-      'User-agent': 'Mozilla/5.0'
-    }
-    
-    params = {
-      'limit': 10
-    }
-    
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
     response = get(url, headers=user_agent, params=params)
